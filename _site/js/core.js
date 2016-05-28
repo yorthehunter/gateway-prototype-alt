092a8c4f6b8e11f073e1850755484f71085d2899
fuselagetown <torunn@skrogstad.no>
2016-05-27 19:27:50 -0700

    v1

---
+++ _site/js/core.js
@@ -0,0 +1,68 @@
+
+$(document).ready(function(){
+    $(".video").fitVids();
+});
+
+
+
+$(function() {
+
+
+
+  $('#toggle').click(function(){
+    $('nav.main').slideToggle( "fast" );
+    $('#toggle').toggleClass('active');
+    return false;
+  });
+
+
+
+  $('.divet-link').click(function(){
+    $('.toggleable-gender').slideToggle( "fast" );
+    return false;
+  });
+
+
+// set content on click
+  $('.button').click(function(e) {
+      e.preventDefault();
+      setContent($(this));
+  });
+
+  // set content on load
+  $('.button.active').length && setContent($('.button.active'));
+
+  function setContent($el) {
+      $('.button').removeClass('active');
+      $('.container').hide();
+
+      $el.addClass('active');
+      $($el.data('rel')).show();
+  }
+
+  $('#get-started').click(function(){
+            //$(this).animate(function(){
+                $('html, body').animate({
+                    scrollTop: $("#get-started-form").offset().top
+                     }, 700);
+            //});
+    return false;
+  });
+
+  $('.get-started').click(function(){
+            //$(this).animate(function(){
+                $('html, body').animate({
+                    scrollTop: $("#get-started-form").offset().top
+                     }, 700);
+            //});
+    return false;
+  });
+
+
+
+
+
+});
+
+
+
