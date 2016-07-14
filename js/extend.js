0a62634e41f1e285ff16ea42e462794a4fa70308
Eric Sipple <esipple@stitchfix.com>
2016-07-14 12:28:09 -0400

    Simple javascript to hide and show the right uI elements.

--- js/extend.js
+++ js/extend.js
@@ -6,4 +6,10 @@ $(function() {
     console.log("clicked")
   })
 
+  $("#more-needed").click(function(e){
+    e.preventDefault();
+    $("#default-duration").hide();
+    $("#calendar-duration").show();
+  });
+
 });
